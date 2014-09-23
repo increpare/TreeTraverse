122afc478a10eb5ee3c696a2a35a3746779f1f2c
Stephen Lavelle <analytic@gmail.com>
2014-09-23 17:55:04 +0100

    interface outline
    interface outline

---
+++ TreeTraverse/Abstract/Outline.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+public class Outline
+{
+
+	public interface MyState {
+		void Load(string s);
+		void Propagate();
+		void Presume(Assumption a);
+		Edge UnspecifiedPassiveEdge();
+		Assumption[] GenerateAssumptions(Edge E);
+		MyState Clone();
+		bool Finished();
+		void SetState(MyState other);
+	}
+
+	public interface Graph {
+	}
+
+	public interface Edge {
+	}
+
+	public interface Assumption {
+	}
+
+	public void RunAlgorithm(MyState state) {
+		Edge e = state.UnspecifiedPassiveEdge();
+		if (e == null) {
+			return;
+		} 
+
+		var assumptions = state.GenerateAssumptions (e);
+
+		MyState[] newStates = new MyState[3];
+		for (int i = 0; i < 2; i++) {
+			var newState = state.Clone ();
+			newState.Presume (assumptions [i]);
+			newState.Propagate ();		
+		}
+	}
+
+	public void DoStuff(MyState state) {
+		state.Propagate();
+		RunAlgorithm (state,null);
+	}
+}
+
